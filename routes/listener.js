2a8993b0bd6282d5fba1c7d80da43e97898443fb
Vuong Ken <ken.vuong@danaexperts.com>
2024-07-21 18:47:27 +0700

    update

--- routes/listener.js
+++ routes/listener.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/alert-hook", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   return res.status(200).json({
-    data: req
+    data: req.body
   });
 });
 
