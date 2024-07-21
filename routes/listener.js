3a12f39bd795ee2e8f4f3dfc48a7d8770f234725
Vuong Ken <ken.vuong@danaexperts.com>
2024-07-21 18:58:45 +0700

    update

--- routes/listener.js
+++ routes/listener.js
@@ -4,16 +4,7 @@ const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   const data = req.body;
-  const filePath = "./data.json";
-
-  fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
-    if (err) {
-      console.error("Error writing to file", err);
-      return res.status(500).json({ error: "Failed to write to file" });
-    }
-    console.log("Successfully wrote to file");
-    return res.status(200).json({ message: "Data saved", data: data });
-  });
+  return res.status(200).json({ data: data });
 });
 
 module.exports = router;
