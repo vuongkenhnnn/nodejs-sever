748968c3b85e5e55b877328140ed32e292ea37d0
Vuong Ken <ken.vuong@danaexperts.com>
2024-07-21 18:54:43 +0700

    update

--- routes/listener.js
+++ routes/listener.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   const data = req.body;
-  const filePath = "data.json";
+  const filePath = "./data.json";
 
   fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
     if (err) {
